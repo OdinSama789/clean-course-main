b49ee6741ef74bcdc2e71d3d6fa306098fe0dfa9
Giovanni Alexis Aguirre Dávalos <202213854k@utea.edu.pe>
2026-06-09 09:27:31 -0500

    Mejorados nombres descriptivos de variables

--- src/clean-code/02-name-types.ts
+++ src/clean-code/02-name-types.ts
@@ -1,61 +1,28 @@
-(() => {
+// arreglo de temperaturas celsius
+const celsiusTemperatures = [33.6, 12.34];
 
-    // arreglo de temperaturas celsius
-    const arrayOfNums = [33.6, 12.34];
+// Dirección ip del servidor
+const serverIp = '123.123.123.123';
 
-    // Dirección ip del servidor
-    const ip = '123.123.123.123';
+// Listado de usuarios
+const users = [
+    { id: 1, email: 'fernando@google.com' },
+    { id: 2, email: 'juan@google.com' },
+    { id: 3, email: 'melissa@google.com' }
+];
 
-    // Listado de usuarios
-    const people = [{id: 1, email: 'fernando@google.com'},{ id: 2, email: 'juan@google.com' }, { id: 3, email: 'melissa@google.com' }];
+// Listado de emails de los usuarios
+const userEmails = users.map( user => user.email );
 
-    // Listado de emails de los usuarios
-    const emails = people.map( u => u.email );
+// Variables booleanas de un video juego
+const canJump = false;
+const canRun = true;
+const hasNoItems = true;
+const isLoading = false;
 
-    // Variables booleanas de un video juego
-    const jump = false;
-    const run = true;
-    const noTieneItems = true;
-    const loading = false;
+// tiempo inicial
+const startTime = new Date().getTime();
 
-    // Otros ejercicios
-    // tiempo inicial
-    const start = new Date().getTime();
-    //....
-    // 3 doritos después
-    //...
-    // Tiempo al final
-    const end = new Date().getTime() - start;
-
-
-    // Funciones
-    // Obtiene los libros
-    function book() {
-        throw new Error('Function not implemented.');
-    }
-
-    // obtiene libros desde un URL
-    function BooksUrl( u: string) {
-        throw new Error('Function not implemented.');
-    }
-    
-    // obtiene el área de un cuadrado basado en sus lados
-    function areaCuadrado( s: number ) {
-        throw new Error('Function not implemented.');
-    }
-
-    // imprime el trabajo
-    function printJobIfJobIsActive() {
-        throw new Error('Function not implemented.');
-    }
-    
-    
-
-
-
-
-})();
-
-
-
+// Tiempo al final
+const elapsedTime = new Date().getTime() - startTime;
 
